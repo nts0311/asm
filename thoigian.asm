04c49bb41e3b220856cec750615c853ed2bd4d6a
ExpandingMilkBox <61538893+nts0311@users.noreply.github.com>
2020-10-21 00:20:43 +0700

    aa

--- thoigian.asm
+++ thoigian.asm
@@ -4,10 +4,13 @@ data segment
     ; add your data here!
     pkey db "press any key...$"
     nl db 13,10,'$'
-    soPhut dW 103
+    soPhut dW 5
     gioKetThuc db 0
     phutKetThuc db 0
-    giayKetThuc db 0
+    giayKetThuc db 0 
+    
+    phutDaQua dw 0
+    phutTruoc db 0
     
 
 ends
@@ -25,32 +28,76 @@ start:
 
     ; add your code here
     
+    ;mov ah, 2ch
+;    int 21h
+;    mov gioKetThuc, ch
+;    mov phutKetThuc, cl
+;    mov giayKetThuc, dh
+;    
+;    xor dh, dh
+;    mov dl, 60
+;    mov ax, soPhut
+;    div dl
+;   
+;    add gioKetThuc, al 
+;    
+;    
+;    add phutKetThuc, ah
+;          
+;    xor ah, ah      
+;    mov al, phutKetThuc
+;    div dl
+;    add gioKetThuc, al
+;    mov phutKetThuc,ah 
+;    
+;    
+;lap:
+;    mov ah, 2ch
+;    int 21h
+;    
+;    cmp ch, gioKetThuc
+;    je so_sanh_phut
+;    jmp lap
+;    
+;so_sanh_phut:
+;    cmp cl, phutKetThuc
+;    je so_sanh_giay
+;    jmp lap
+;    
+;so_sanh_giay:
+;    cmp dh, giayKetThuc
+;    je exit
+;    jmp lap    
+    
+    
     mov ah, 2ch
     int 21h
-    mov gioKetThuc, ch
-    mov phutKetThuc, cl
-    mov giayKetThuc, dh
-    
-    xor dh, dh
-    mov dl, 60
-    mov ax, soPhut
-    div dl
+    mov phutTruoc, cl
+    mov giayKetThuc, dh 
    
-    add gioKetThuc, al 
+lap: 
+    int 21h
+    cmp phutTruoc, cl
+    jne tang_so_phut
+    
+    mov bx, soPhut
+    cmp phutDaQua,bx
+    jae so_sanh_giay
+    
+    jmp lap
+    
+tang_so_phut:
+    inc phutDaQua 
+    mov phutTruoc, cl
+    jmp lap
     
     
-    add phutKetThuc, ah
-          
-    xor ah, ah      
-    mov al, phutKetThuc
-    div dl
-    add gioKetThuc, al
-    mov phutKetThuc,ah
+so_sanh_giay:
+    cmp dh, giayKetThuc
+    je exit
+    jmp lap
     
-    
-   
-   
-   
+       
   
         
 exit:    
